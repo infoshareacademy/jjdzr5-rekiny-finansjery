555183b005cfc06f8ce2bc094cbd555f52116114
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2022-02-18 22:35:30 +0100

    Menu class initial commit

---
+++ src/main/java/com/infoshareacademy/presentationlayer/Menu.java
@@ -0,0 +1,15 @@
+package com.infoshareacademy.presentationlayer;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void menu(){
+        Scanner scanner = new Scanner(System.in);
+        int menuChoice = scanner.nextInt();
+
+        String[] menuOptions = new String[5];
+
+    }
+
+}
