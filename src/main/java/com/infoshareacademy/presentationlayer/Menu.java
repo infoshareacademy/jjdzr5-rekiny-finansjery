6bffadb1cd9be65b1998fec7e1493f3aa7c84fe5
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2022-02-23 18:06:06 +0100

    Exception handling implemented in the Menu class

--- src/main/java/com/infoshareacademy/presentationlayer/Menu.java
+++ src/main/java/com/infoshareacademy/presentationlayer/Menu.java
@@ -9,52 +9,50 @@ public class Menu {
 
 
         String[] menuOptions = {"Option 1", "Option 2", "Option 3"};
-        String[] menuSubOptions = {"Sub-option 1", "Sub-option 2", "Sub-option 3"};
+        String[] option1SubOptions = {"Sub-option 1", "Sub-option 2", "Sub-option 3"};
 
 
         for (int i = 0; i < menuOptions.length; i++) {
             System.out.println(menuOptions[i]);
         }
         System.out.println("\n");
-        System.out.println("Please choose the desired menu option and press <ENTER>:");
-
-        boolean choiceIsBad = false;
-        while(!choiceIsBad) {
+        boolean isInt = false;
+        do {
             try {
+                System.out.println("Please choose the desired menu option and press <ENTER>:");
                 int menuChoice = scanner.nextInt();
-
+                isInt = true;
                 switch (menuChoice) {
                     case 1:
                         System.out.println("You've chosen " + menuOptions[0]);
                         System.out.println("The available sub-options are: ");
-                        for (int i = 0; i < menuSubOptions.length; i++) {
-                            System.out.println(menuSubOptions[i]);
+                        for (int i = 0; i < option1SubOptions.length; i++) {
+                            System.out.println(option1SubOptions[i]);
                         }
                         System.out.println("\n");
                         System.out.println("Please choose your sub-option:");
-
                         try {
+                            isInt = false;
                             menuChoice = scanner.nextInt();
+                            isInt = true;
                             switch (menuChoice) {
                                 case 1:
-                                    System.out.println("You've chosen " + menuSubOptions[0]);
+                                    System.out.println("You've chosen " + option1SubOptions[0]);
                                     break;
                                 case 2:
-                                    System.out.println("You've chosen " + menuSubOptions[1]);
+                                    System.out.println("You've chosen " + option1SubOptions[1]);
                                     break;
                                 case 3:
-                                    System.out.println("You've chosen " + menuSubOptions[2]);
+                                    System.out.println("You've chosen " + option1SubOptions[2]);
                                     break;
                                 default:
                                     System.out.println("You've selected an invalid menu option. Please try again.");
-                                    choiceIsBad = true;
-                                    System.out.println(choiceIsBad);
+                                    isInt = false;
                             }
                         } catch (Exception e) {
                             System.out.println("The input must be a number corresponding to the menu option.");
-                            choiceIsBad = true;
+                            scanner.next();
                         }
-
                         break;
                     case 2:
                         System.out.println("You've chosen " + menuOptions[1]);
@@ -64,13 +62,13 @@ public class Menu {
                         break;
                     default:
                         System.out.println("You've selected an invalid menu option. Please try again.");
-                        choiceIsBad = true;
+                        isInt = false;
                 }
             } catch (Exception e) {
                 System.out.println("The input must be a number corresponding to the menu option.");
-                choiceIsBad = true;
+                scanner.next();
             }
-        }
+        } while(!isInt);
 
     }
 
